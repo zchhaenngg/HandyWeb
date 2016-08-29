63cd9c846fb8c145b1d3b5676e9ffdd8020656a6
zchhaenngg <1076968967@qq.com>
2016-08-29 20:22:19 +0800

    enum t.HasFalag(t.a)

--- HandyWork.DAL/Program.cs
+++ HandyWork.DAL/Program.cs
@@ -12,6 +12,26 @@ using System.Threading.Tasks;
 
 namespace HandyWork.DAL
 {
+    public enum t: int
+    {
+        a = 1,
+        b = 2,
+        c = 4
+    }
+
+    public class EnumWrapper<TEnum>
+    {
+        private TEnum _tEnum;
+        public EnumWrapper(TEnum tEnum)
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new Exception(string.Format("{0} 必须为枚举类型", typeof(TEnum).Name));
+            }
+            _tEnum = tEnum;
+        }
+    }
+
     public class Program
     {
         static void a([NotNull]string d)
@@ -19,10 +39,37 @@ namespace HandyWork.DAL
             Check.NotNull(d, nameof(d));
             Console.WriteLine("aaa");
         }
+
+        static bool IsT(t value,t target)
+        {
+            return (value & target) > 0;
+        }
+
+        static void ttt(t t1)
+        {
+            if (t1.HasFlag(t.a))
+            {
+                Console.WriteLine("a");
+            }
+            if (t1.HasFlag(t.b))
+            {
+                Console.WriteLine("b");
+            }
+            if (t1.HasFlag(t.c))
+            {
+                Console.WriteLine("c");
+            }
+        }
         static void Main(string[] args)
         {
-            // populate and print data
-            a(null);
+            t t1 = t.a;
+            t t2 = t.b;
+            t t3 = t1 | t2;
+            ttt(t3);
+            Console.WriteLine(t3);
+            int d = 1 | 2;
+            int d2 = 1 & 3;
+            Console.WriteLine(d);
             Console.ReadLine();
         }
     }
