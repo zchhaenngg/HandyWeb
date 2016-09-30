6f377677b3d8e8818aae45db5e040b03c568527d
zchhaenngg <1076968967@qq.com>
2016-09-30 21:05:27 +0800

    UTCTime的Value始终会是UTC时间

--- HandyWork.Commom.UnitTests/Time/UTCTimeTest.cs
+++ HandyWork.Commom.UnitTests/Time/UTCTimeTest.cs
@@ -10,36 +10,76 @@ namespace HandyWork.Commom.UnitTests.UTC
         [TestMethod]
         public void UTCTime_Test()
         {
-            //UTCTime ut = null;  编译失败
-            UTCTime time = new UTCTime();
-            var a = time.Value;
+            {//1.test是否为值类型 
+                UTCTime time = new UTCTime{ Value = DateTime.Now };
+                UTCTime time2 = time;
 
-            UTCTime time2 = time;
-            time2.Value = DateTime.Now;
-            //1.测试是否为值类型
-            Assert.IsTrue(time.Value != time2.Value);
-            //2.测试可空类型
-            UTCTime? t = null;
-            Assert.IsFalse(t.HasValue);
-            t = DateTime.Now;
-            Assert.IsTrue(t.HasValue);
-            //3.测试非可识别的时间类型转换成UTC时间应报错。
-            try
-            {
-                UTCTime t2 = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified);
-                Assert.IsTrue(false);
+                time2.Value = DateTime.Now;
+                time2.Value = DateTime.MinValue;
+                Assert.IsTrue(time.Value != time2.Value);
             }
-            catch (InvalidCastException)
-            {
-                Assert.IsTrue(true);
+
+            {//2.test可空类型
+                UTCTime? t = null;
+                Assert.IsFalse(t.HasValue);
+                t = DateTime.Now;
+                Assert.IsTrue(t.HasValue);
             }
-            //test初试时间的时间类型
-            UTCTime ut = new UTCTime();
-            Assert.IsTrue(ut.Value.Kind == DateTimeKind.Utc);
+
+            {//3.test非可识别的时间类型转换成UTC时间应报错。
+                try
+                {
+                    UTCTime t2 = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified);
+                    Assert.IsTrue(false);
+                }
+                catch (InvalidCastException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            {//4.test初试时间的时间类型
+                UTCTime ut = new UTCTime();
+                Assert.IsTrue(ut.Value.Kind == DateTimeKind.Utc);
+            }
+
+            {//5.datetime->utctime
+                UTCTime ut = DateTime.Now;
+                UTCTime? ut2 = DateTime.Now;
+
+                DateTime? time = DateTime.Now;
+                UTCTime ut3 = (UTCTime)time;
+
+                try
+                {
+                    UTCTime ut4 = (UTCTime)null;
+                    Assert.IsTrue(false);
+                }
+                catch (InvalidCastException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            {//6.utctime->datetime
+                DateTime dt = UTCTime.Now;
+                DateTime? dt2 = UTCTime.Now;
+                UTCTime? ut = UTCTime.Now;
+                DateTime dt3 = (DateTime)ut;
+
+                try
+                {
+                    ut = null;
+                    DateTime dt4 = (DateTime)ut;
+                }
+                catch (InvalidCastException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+
         }
 
         [TestMethod]
-        public void UTCTime_Test2()
+        public void UTCTime_Parse()
         {
             string s = "2016-09-27";
             DateTime t;
--- HandyWork.Common/Time/UTCTime.cs
+++ HandyWork.Common/Time/UTCTime.cs
@@ -6,36 +6,105 @@ using System.Threading.Tasks;
 
 namespace HandyWork.Common.Time
 {
+    /// <summary>
+    /// UTCTime的Value始终会是UTC时间
+    /// </summary>
     public struct UTCTime
     {
         private DateTime _datetime;
 
-        public UTCTime(DateTime utc): this()
+        public UTCTime(DateTime time): this()
         {
-            Value = utc;
+            Value = time;
         }
 
         public DateTime Value
         {
             get
             {
-                return _datetime.ToUniversalTime();
+                if (_datetime == DateTime.MinValue)
+                {//默认值也要转换为UTC时间
+                    _datetime = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc);
+                }
+                return _datetime;
             }
             set
             {
+                
                 if (value.Kind == DateTimeKind.Unspecified)
                 {
-                    throw new InvalidCastException("Unspecified 时间无法转换成UTC时间");
+                    if (value == DateTime.MinValue)
+                    {//默认值也要转换为UTC时间
+                        value = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc);
+                    }
+                    else
+                    {
+                        throw new InvalidCastException("Unspecified 时间无法转换成UTC时间");
+                    }
                 }
                 _datetime = value.ToUniversalTime();
             }
         }
-
-        public static implicit operator UTCTime(DateTime utctime)
-        {
-            return new UTCTime { Value = utctime };
-        }
-
+        
         public static UTCTime Now => new UTCTime(DateTime.UtcNow);
+
+        //datetime->UTCTime
+        public static implicit operator UTCTime(DateTime time)
+        {
+            return new UTCTime { Value = time };
+        }
+        public static implicit operator UTCTime?(DateTime? time)
+        {
+            if (time == null)
+            {
+                return null;
+            }
+            else
+            {
+                return time.Value;
+            }
+            
+        }
+        public static explicit operator UTCTime(DateTime? time)
+        {
+            if (time == null)
+            {
+                throw new InvalidCastException("time为空无法转换成UTCTime");
+            }
+            else
+            {
+                return time.Value;
+            }
+
+        }
+        //UTCTime->datetime
+        public static implicit operator DateTime(UTCTime time)
+        {
+            return time.Value;
+        }
+        public static implicit operator DateTime? (UTCTime? time)
+        {
+            if (time == null)
+            {
+                return null;
+            }
+            else
+            {
+                return time.Value;
+            }
+
+        }
+        public static explicit operator DateTime(UTCTime? time)
+        {
+            if (time == null)
+            {
+                throw new InvalidCastException("time为空无法转换成DateTime");
+            }
+            else
+            {
+                return time.Value;
+            }
+
+        }
     }
 }
