ea7e63baf2471795ee666a484dad3a3ad5c4ebce
zchhaenngg <1076968967@qq.com>
2016-11-06 03:03:09 +0800

    在分页控件上学习使用angular

---
+++ HandyWork.ViewModel/Web/BootstrapViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandyWork.ViewModel.Web
+{
+    public class BootstrapPagination
+    {
+        public long Tickets { get; } = DateTime.Now.Ticks;
+
+        private string _app;
+        public string app => _app ?? (_app = "pagination_app_" + Tickets);
+
+        private string _controller;
+        public string controller => _controller ?? (_controller = "pagination_controller_" + Tickets);
+
+        public string url { get; set; }
+    }
+}
--- HandyWork.Web/App_Start/BundleConfig.cs
+++ HandyWork.Web/App_Start/BundleConfig.cs
@@ -23,9 +23,13 @@ namespace HandyWork.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
