99d690093104567e732971dc4f8545f13635fe2b
zchhaenngg <1076968967@qq.com>
2016-11-06 17:19:50 +0800

    Razor和angular结合

--- HandyWork.ViewModel/Web/BootstrapViewModel.cs
+++ HandyWork.ViewModel/Web/BootstrapViewModel.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace HandyWork.ViewModel.Web
 {
-    public class BootstrapPagination
+    public partial class BootstrapPagination
+    {//必须返回给页面
+        public string url { get; set; }
+        /// <summary>
+        /// 找到的查询结果总数
+        /// </summary>
+        public int total_count { get; set; }
+    }
+    /// <summary>
+    /// default设置
+    /// </summary>
+    public partial class BootstrapPagination
     {
         public long Tickets { get; } = DateTime.Now.Ticks;
 
@@ -15,7 +26,11 @@ namespace HandyWork.ViewModel.Web
 
         private string _controller;
         public string controller => _controller ?? (_controller = "pagination_controller_" + Tickets);
-
-        public string url { get; set; }
+        
+        public int repeat { get; set; } = 7;
+        
+        public int page_size { get; set; } = 10;
+        
+        public int max_page_number => total_count / page_size + total_count % page_size > 0 ? 1 : 0;
     }
 }
