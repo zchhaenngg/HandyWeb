21a63ac22b10e9c95fce7876a29627f913c294cc
zchhaenngg <1076968967@qq.com>
2016-12-18 13:17:55 +0800

    upgrade

---
+++ HandyWork.Web/Scripts/typescripts/hy-1.0/hy_query.js
@@ -0,0 +1,16 @@
+var AndOr;
+(function (AndOr) {
+    AndOr[AndOr["And"] = 1] = "And";
+    AndOr[AndOr["Or"] = 0] = "Or";
+})(AndOr || (AndOr = {}));
+var HyQueryItem = (function () {
+    function HyQueryItem() {
+    }
+    return HyQueryItem;
+}());
+var HyQuery = (function () {
+    function HyQuery() {
+    }
+    return HyQuery;
+}());
+//# sourceMappingURL=hy_query.js.map
---
+++ HandyWork.Web/Scripts/typescripts/hy-1.0/hy_query.ts
@@ -0,0 +1,13 @@
+﻿enum AndOr {
+    And = 1,
+    Or = 0
+}
+class HyQueryItem {
+    ///运算符“与”或“或”
+    public andor: AndOr;
+}
+class HyQuery {
+    //查询名称
+    public name: string;
+    //public hyQueryItems: List<HyQueryItem>
+}
