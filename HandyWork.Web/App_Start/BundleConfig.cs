3a45d50a5392480a97706496e5e8d8ab06191e0d
zchhaenngg <1076968967@qq.com>
2017-01-08 17:11:02 +0800

    确定bootstrap和easyui结合使用

--- HandyWork.Web/App_Start/BundleConfig.cs
+++ HandyWork.Web/App_Start/BundleConfig.cs
@@ -9,10 +9,13 @@ namespace HandyWork.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             /******************************************StyleBundle********************************************/
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                      "~/Content/bootstrap.css"
+                ));
             bundles.Add(new StyleBundle("~/Content/easyui").Include(
                       "~/Scripts/jquery-easyui-1.5.1/themes/default/easyui.css",
                       "~/Scripts/jquery-easyui-1.5.1/themes/icon.css"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
             /******************************************ScriptBundle********************************************/
@@ -21,6 +24,9 @@ namespace HandyWork.Web
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/easyui").Include(
                    "~/Scripts/jquery-easyui-1.5.1/plugins/jquery.parser.js",
